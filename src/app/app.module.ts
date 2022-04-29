6f22484a220867a13b1e398a4ac31487fe3ad751
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-29 14:14:44 +0300

    layout-optimization for page two

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { BodyComponent } from './Components/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OneComponent,
-    TwoComponent
+    TwoComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
